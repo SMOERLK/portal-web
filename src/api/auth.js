add8a3867e27bedf2ab768e882f0b28ccb4616c9
Mohamed Nizar <nizaucsc@gmail.com>
2020-12-07 19:02:26 +0530

    Init react app for addtional data collection tool

---
+++ src/api/auth.js
@@ -0,0 +1,94 @@
+import defaultUser from '../utils/default-user';
+import axios from 'axios';
+
+export const URL = "http://127.0.0.1:8080/";
+export const API_URL = "http://127.0.0.1:8080/api";
+export const my_app = axios.create({baseURL: API_URL});
+
+export async function signIn(username, password) {
+  try {
+    const api = {
+      baseUrl:
+    }
+    // Send request
+    console.log(username, password);
+
+    return {
+      isOk: true,
+      data: defaultUser
+    };
+  }
+  catch {
+    return {
+      isOk: false,
+      message: "Authentication failed"
+    };
+  }
+}
+
+export async function getUser() {
+  try {
+    // Send request
+
+    return {
+      isOk: true,
+      data: defaultUser
+    };
+  }
+  catch {
+    return {
+      isOk: false
+    };
+  }
+}
+
+export async function createAccount(email, password) {
+  try {
+    // Send request
+    console.log(email, password);
+
+    return {
+      isOk: true
+    };
+  }
+  catch {
+    return {
+      isOk: false,
+      message: "Failed to create account"
+    };
+  }
+}
+
+export async function changePassword(email, recoveryCode) {
+  try {
+    // Send request
+    console.log(email, recoveryCode);
+
+    return {
+      isOk: true
+    };
+  }
+  catch {
+    return {
+      isOk: false,
+      message: "Failed to change password"
+    }
+  }
+}
+
+export async function resetPassword(email) {
+  try {
+    // Send request
+    console.log(email);
+
+    return {
+      isOk: true
+    };
+  }
+  catch {
+    return {
+      isOk: false,
+      message: "Failed to reset password"
+    };
+  }
+}
---
+++ src/components/login-form/login-form.js
@@ -0,0 +1,88 @@
+import React, { useState, useRef, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Form, {
+  Item,
+  Label,
+  ButtonItem,
+  ButtonOptions,
+  RequiredRule,
+  EmailRule
+} from 'devextreme-react/form';
+import LoadIndicator from 'devextreme-react/load-indicator';
+import notify from 'devextreme/ui/notify';
+import { useAuth } from '../../contexts/auth';
+
+import './login-form.scss';
+
+export default function () {
+  const history = useHistory();
+  const { signIn } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const formData = useRef({});
+
+  const onSubmit = useCallback(async (e) => {
+    e.preventDefault();
+    const { username, password } = formData.current;
+    setLoading(true);
+
+    const result = await signIn(username, password);
+    if (!result.isOk) {
+      setLoading(false);
+      notify(result.message, 'error', 2000);
+    }
+  }, [signIn]);
+
+  const onCreateAccountClick = useCallback(() => {
+    history.push('/create-account');
+  }, [history]);
+
+  return (
+    <form className={'login-form'} onSubmit={onSubmit}>
+      <Form formData={formData.current} disabled={loading}>
+        <Item
+          dataField={'username'}
+          editorType={'dxTextBox'}
+          editorOptions={emailEditorOptions}
+        >
+          <RequiredRule message="Username is required" />
+          {/* <EmailRule message="Username is invalid" /> */}
+          <Label visible={false} />
+        </Item>
+        <Item
+          dataField={'password'}
+          editorType={'dxTextBox'}
+          editorOptions={passwordEditorOptions}
+        >
+          <RequiredRule message="Password is required" />
+          <Label visible={false} />
+        </Item>
+        <Item
+          dataField={'rememberMe'}
+          editorType={'dxCheckBox'}
+          editorOptions={rememberMeEditorOptions}
+        >
+          <Label visible={false} />
+        </Item>
+        <ButtonItem>
+          <ButtonOptions
+            width={'100%'}
+            type={'default'}
+            useSubmitBehavior={true}
+          >
+            <span className="dx-button-text">
+              {
+                loading
+                  ? <LoadIndicator width={'24px'} height={'24px'} visible={true} />
+                  : 'Sign In'
+              }
+            </span>
+          </ButtonOptions>
+        </ButtonItem>
+      </Form>
+    </form>
+  );
+}
+
+const emailEditorOptions = { stylingMode: 'filled', placeholder: 'Username', mode: 'text' };
+const passwordEditorOptions = { stylingMode: 'filled', placeholder: 'Password', mode: 'password' };
+const rememberMeEditorOptions = { text: 'Remember me', elementAttr: { class: 'form-text' } };
