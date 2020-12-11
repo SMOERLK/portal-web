29f3dfd6aad310f666b7b7bc99ea50494b854d40
Sennen Randika <randikasennen@gmail.com>
2020-12-11 14:39:42 +0530

    Implemented response interceptor for API calls

--- src/api/api.js
+++ src/api/api.js
@@ -8,4 +8,32 @@ const API = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
+
+// Response interceptor for API calls
+API.interceptors.response.use((response) => {
+  return response
+},
+async function (error) {
+  const originalRequest = error.config;
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    const response = await API.put('/refresh', {});
+
+    if (response.status === 201) {
+      const refreshedToken = response.data.token;
+      
+      var user = JSON.parse(localStorage.getItem('user'));
+      user.token = refreshedToken;
+      localStorage.setItem('user', JSON.stringify(user));
+
+      API.defaults.headers.common['Authorization'] = 'Bearer ' + refreshedToken;
+
+      return API(originalRequest);
+    }
+  }
+
+  return Promise.reject(error);
+});
+
+
 export default API;
