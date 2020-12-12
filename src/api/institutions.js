5fdba7352b60ce3a10913ad51743b6f32a589c1f
Sennen Randika <randikasennen@gmail.com>
2020-12-12 08:48:32 +0530

    Converted to async function

--- src/api/institutions.js
+++ src/api/institutions.js
@@ -1,7 +1,8 @@
 import API from './api';
 
-export function getInstitutions() {
+export async function getInstitutions() {
   const url = '/institutions?limit=10&page=1';
-
-  return API.get(url).then((response) => { return response.data.data })
+  const response = await API.get(url);
+  
+  return response.data.data;
 }
